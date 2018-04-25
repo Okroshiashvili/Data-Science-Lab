332e608fcd4b0f6796549d98979d495a0611576d
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-04-25 18:17:18 +0400

    Add files via upload

---
+++ Model Selection/Grid Search.py
@@ -0,0 +1,189 @@
+"""
+Created on Wed Mar 28 21:10:40 2018
+
+@author: Nodar.Okroshiashvili
+"""
+
+"""
+Grid Search
+
+"""
+
+#%%
+
+
+# Pre-processing of data
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#%%
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+# Independent variables
+X = dataset.iloc[:, [2,3]].values
+
+# Dependent variable
+y = dataset.iloc[:, 4].values
+
+#%%
+
+from sklearn.model_selection import train_test_split
+
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+#%%
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+# Define scaler object
+sc_X = StandardScaler()
+
+# Apply scaler object to the train and test set
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#%%
+
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+
+# Define classifier object
+classifier = SVC(kernel='rbf', random_state = 0)
+
+# Fit the model
+classifier.fit(X_train, y_train)
+
+
+#%%
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+#%%
+
+# Make the Confusion Matrix
+
+from sklearn.metrics import confusion_matrix
+
+# Create confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+#%%
+
+"""
+Applying K-Fold Cross Validation
+
+"""
+
+from sklearn.model_selection import cross_val_score
+
+
+# We define accuracy vector which will be populated after K-Fold
+accuracies = cross_val_score(estimator=classifier, X = X_train, y = y_train, cv = 10)
+
+# Get average accuracy
+mean = accuracies.mean()
+# Get standard deviation
+std = accuracies.std()
+
+#%%
+
+"""
+Applying Grid Search
+
+"""
+
+from sklearn.model_selection import GridSearchCV
+
+
+# Identify parameter which have to be optimized
+parameters = [{'C':[1, 10, 100, 1000], 'kernel':['linear']},
+               {'C':[1,10,100,1000],'kernel':['rbf'], 'gamma':[0.5,0.1,0.01,0.001]}
+               ]
+
+
+# Create object
+grid_search = GridSearchCV(estimator=classifier, param_grid = parameters,
+                           scoring='accuracy',
+                           cv = 10,
+                           n_jobs = -1)
+
+
+
+# Fit to the training set
+grid_search = grid_search.fit(X_train, y_train)
+
+
+# Let see the grid search result
+best_accuracy = grid_search.best_score_
+
+
+# See the best parameters optimized by grid search
+best_parameters = grid_search.best_params_
+
+
+#%%
+
+
+# Visualizing the Training set results
+
+from matplotlib.colors import ListedColormap
+
+# Re-define variables
+X_set, y_set = X_train, y_train
+
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM Classifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#%%
+
+
+# Visualizing the Test set results
+
+from matplotlib.colors import ListedColormap
+
+# Re-define variables
+X_set, y_set = X_test, y_test
+
+X1, X2 = np.meshgrid(np.arange(start = X_set[:,0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+
