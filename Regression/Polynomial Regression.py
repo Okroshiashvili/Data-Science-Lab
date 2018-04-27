bbdcc45bdeae66d1ca20a9a6bbbb5065cd540080
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-04-27 15:16:38 +0400

    Add files via upload

---
+++ Regression/Polynomial Regression.py
@@ -0,0 +1,89 @@
+"""
+Created on Fri Apr 27 14:59:03 2018
+
+@author: Nodar.Okroshiashvili
+"""
+
+"""
+
+ Polynomial Regression
+
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data/Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#%%
+
+
+# I don't split dataset into training and test set and even didn't do feature scaling
+
+
+
+# Fitting Linear Regression to the dataset
+# This is just for comparison purposes
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+
+# Fitting Polynomial Regression to the dataset
+
+from sklearn.preprocessing import PolynomialFeatures
+
+poly_reg = PolynomialFeatures(degree = 4)
+# Applies 4th degree polynomial
+ 
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+#%%
+
+# Visualizing the Linear Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Linear Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#%%
+
+# Visualizing the Polynomial Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#%%
+
+# Visualization the Polynomial Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#%%
+
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5) 
+# 6.5 is taken randomly. Take whatever you want
+
+# Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
