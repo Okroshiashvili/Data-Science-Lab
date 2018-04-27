90ec10d70428545dd24ddaae6e37c71f43b00db3
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-04-27 15:35:34 +0400

    Add files via upload

---
+++ Regression/Decision Tree Regression.py
@@ -0,0 +1,81 @@
+"""
+Created on Fri Apr 27 15:29:27 2018
+
+@author: Nodar.Okroshiashvili
+"""
+
+"""
+
+ Decision Tree Regression
+
+"""
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data/Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#%%
+
+# I don't split dataset because it's too small for that
+
+
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+#%%
+
+# Visualizing the Decision Tree Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+"""
+
+We have to rise red flag because Decision Tree algorithm splits the data in rectangular forms in case of two or more variable. 
+Since we have here one independent variable algorithm uses intervals.
+Form the graph it's clear that algorithm uses either infinite intervals or there is other problem.
+We don't have first problem, because it impossible to use infinite interval between two points,
+so we have another problem.
+The problem here is that the Decision Tree algorithm in not continuous.
+
+
+"""
+
+
+#%%
+
+# Visualizing the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+"""
+
+This is the TRUE regression line.
+All the points between 5.5 and 6.5 predicted values will be 150,000 as it is the average of this interval
+
+Generally, Decision Tree model in not interesting in one dimension,
+but in two or more dimensions it's very powerful.
+
+"""
