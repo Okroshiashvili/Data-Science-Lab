9bc5fc79a253bb35b2d3ba0c8e35e51524be6f58
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-04-27 13:35:00 +0400

    Update XGBoost.py

--- Boosting/XGBoost.py
+++ Boosting/XGBoost.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('data/Churn_Modelling.csv')
 
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
